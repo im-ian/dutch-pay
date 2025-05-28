ad724a474268e5c5eeb17a20ce34ea7995df9d83
daeho im <im-ian@naver.com>
2025-05-28 14:19:38 +0900

    Clean up: Remove unused files and update index route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router'
-import logo from '../logo.svg'
-import { DutchPayTable } from "../components/DutchPayTable";
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -10,7 +8,6 @@ function App() {
   return (
     <div className="container mx-auto py-8">
       <h1 className="text-3xl font-bold mb-8">Dutch Pay Calculator</h1>
-      <DutchPayTable />
     </div>
   )
 }
